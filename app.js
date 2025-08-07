11f2193b979b481bb27c252c8873dc475f41957d
Samuel Martinez <samuelmartinezruiz43@gmail.com>
2025-08-07 17:42:20 -0500

    Cambio en log

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ function suma (a,b){
     return a + b;
 }
 
-console.log("Proyecto se ejecuto correctamente");
-console.log(toString(suma(17,483)))
+function run(){
+    console.log("Proyecto se ejecuto correctamente");
+}
+
+run()
 
 //Cambio para probar Action
 module.exports = { suma };
